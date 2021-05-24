54294148719c7497ec7afc71b309c74b8d3b255a
My Little Admin <mylittleadmi@gmail.com>
2021-05-24 15:17:29 +0300

    Bugfix: set a longer timeout due to file blocking while PyCharm indexing

--- init.bat
+++ init.bat
@@ -1,7 +1,7 @@
 curl -L --ssl-no-revoke -o winpython.7z --url https://sourceforge.net/projects/winpython/files/WinPython_3.8/3.8.9.0/Winpython64-3.8.9.0dot.exe/download
 winpython.7z -y -gm2
 del winpython.7z
-timeout 5
+timeout 10
 ren WPy64-3890 WinPython
 WinPython\python-3.8.9.amd64\python.exe -m venv .\venv
 call venv\Scripts\activate.bat
